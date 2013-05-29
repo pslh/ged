ab007cddcb2756aaa9a0f089190c1ffadba371f1
Paul Henshaw <paul@openquake.org>
2013-05-29 14:04:05 +0200

    More minor updates

--- python/ingest2ph.py
+++ python/ingest2ph.py
@@ -17,8 +17,10 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
 #
+
+#
 # This script was originally authored by Stefano Ferri of the JRC and
-# subsequently modified by Paul Henshaw.
+# subsequently modified by Paul Henshaw.of the GEM Foundation
 #
 import sys, os, psycopg2, getpass
 import datetime
