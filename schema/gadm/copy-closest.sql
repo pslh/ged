e3afc5f734726fbd58bd27531556c2b402826da4
Paul Henshaw <paul.henshaw@globalquakemodel.org>
2013-09-30 16:07:38 +0000

    use temp schema

--- schema/gadm/copy-closest.sql
+++ schema/gadm/copy-closest.sql
@@ -1,1 +1,1 @@
-COPY paul.grid_closest_region FROM '/data/ged/gadm/v2/geo/split2/closest-regions-all.csv' WITH (FORMAT CSV);
+COPY temp.grid_closest_region FROM '/data/ged/gadm/v2/geo/split2/closest-regions-all.csv' WITH (FORMAT CSV);
