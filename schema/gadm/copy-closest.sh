8e56124031cc06a0514e9e091c78f615c9f9de0e
Paul Henshaw <paul.henshaw@globalquakemodel.org>
2013-09-30 15:35:45 +0000

    Renamed copy.sql.  Added copy for closest region

---
+++ schema/gadm/copy-closest.sh
@@ -0,0 +1,2 @@
+date > copy-closest.log
+psql -U paul -d ged -f copy-closest.sql >> copy-closest.log 2>&1
---
+++ schema/gadm/copy-closest.sql
@@ -0,0 +1,1 @@
+COPY paul.grid_closest_region FROM '/data/ged/gadm/v2/geo/split2/closest-regions-all.csv' WITH (FORMAT CSV);
