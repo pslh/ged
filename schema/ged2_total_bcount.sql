c1cfebab41ab5c0ee2715572cfa5b2457d20ad7f
Paul Henshaw <paul@openquake.org>
2013-11-08 15:55:50 +0100

    Able to obtain count per cell by calling select * FROM ged2.total_bcount(2361)

---
+++ schema/ged2_total_bcount.sql
@@ -0,0 +1,36 @@
+--
+-- Obtain grid_id, building count for a given study_region
+--
+DROP FUNCTION IF EXISTS ged2.total_bcount(INTEGER);
+
+
+CREATE OR REPLACE FUNCTION ged2.total_bcount(study_region_id INTEGER)
+  RETURNS TABLE(
+  	grid_id INTEGER, 
+  	total_bldg_count FLOAT, 
+  	total_blgd_area FLOAT) AS
+$BODY$
+DECLARE
+	exposure ged2.exposure_t;
+
+	-- Temporary variables, names start with _
+	_total_bldgs  float;
+	_dwellings_count float;
+BEGIN	
+	RETURN QUERY SELECT
+		exp1.grid_id,
+		SUM(exp1.bldg_count) AS total_bldg_count,
+		SUM(exp1.bldg_area) AS total_blgd_area
+	  FROM (
+		SELECT *
+		  FROM ged2.build_study_region_retrec(2361)
+		) AS exp1
+	 GROUP BY exp1.grid_id;
+END;
+$BODY$
+  LANGUAGE plpgsql 
+  VOLATILE
+  COST 100;
+ALTER FUNCTION ged2.total_bcount(integer)
+  OWNER TO paul;
+
