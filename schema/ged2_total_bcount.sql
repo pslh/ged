1f4e954be3e6b1a5b46f799afee0dbd3df19fbe1
Paul Henshaw <paul@openquake.org>
2013-11-22 15:14:04 +0100

    Added columns for quality

--- schema/ged2_total_bcount.sql
+++ schema/ged2_total_bcount.sql
@@ -16,16 +16,23 @@ CREATE OR REPLACE FUNCTION ged2.total_bldg_count_area(study_region_id INTEGER)
   RETURNS TABLE(
   	grid_id INTEGER, 
   	bldg_count FLOAT, 
+  	bldg_count_quality INTEGER,
   	bldg_area FLOAT,
-  	bldg_cost FLOAT) AS
+  	bldg_area_quality INTEGER,
+  	bldg_cost FLOAT,
+  	bldg_cost_quality INTEGER
+  	) AS
 $BODY$
 DECLARE
 BEGIN	
 	RETURN QUERY SELECT
 		exp1.grid_id,
 		SUM(exp1.bldg_count) AS bldg_count,
+		MIN(exp1.bldg_count_quality) AS bldg_count_quality,
 		SUM(exp1.bldg_area) AS bldg_area,
-		SUM(exp1.bldg_cost) AS bldg_cost
+		MIN(exp1.bldg_area_quality) AS bldg_area_quality,
+		SUM(exp1.bldg_cost) AS bldg_cost,
+		MIN(exp1.bldg_cost_quality) AS bldg_cost_quality
 	  FROM (
 		SELECT *
 		  FROM ged2.build_study_region_retrec(study_region_id)
