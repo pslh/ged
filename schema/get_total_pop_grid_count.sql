f87c2f5dc9f263ab0ff868958c912ed31da9f543
Paul Henshaw <paul.henshaw@globalquakemodel.org>
2014-11-22 13:57:20 +0000

    Update sql to update geographic regions with a NULL tot_pop

--- schema/get_total_pop_grid_count.sql
+++ schema/get_total_pop_grid_count.sql
@@ -1,13 +1,18 @@
+--
+-- For all regions with a null total population
+-- Calculate the total population and update the table
+--
 WITH gr AS (
-  SELECT * FROM ged2.geographic_region 
--- WHERE id IN (111, 131) 
--- LIMIT 3
+  SELECT * FROM ged2.geographic_region WHERE tot_pop IS NULL
 ),
 grid AS (
   SELECT gr.id AS gr_id, (ged2.get_region_grid(gr)).* FROM gr
-)
+),
+totals AS (
 SELECT gr_id, 
   SUM(pop_value) AS total_population, COUNT(*) AS total_grid_count 
-  INTO paul.tot_pop_grid_count
   FROM grid GROUP BY gr_id
-
+)
+UPDATE ged2.geographic_region gr 
+   SET tot_pop_source='GED/GRUMP', tot_pop_date=CURRENT_DATE, tot_pop=totals.total_population 
+  FROM totals WHERE totals.gr_id=gr.id
