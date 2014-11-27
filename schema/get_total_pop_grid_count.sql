8c226620c8d5d9e306ee93d2d6ee5904b8946d94
Paul Henshaw <paul.henshaw@globalquakemodel.org>
2014-11-27 09:32:38 +0000

    Updated to handle single cell regions (e.g. Vatican City)

--- schema/get_total_pop_grid_count.sql
+++ schema/get_total_pop_grid_count.sql
@@ -4,8 +4,10 @@
 --
 WITH gr AS (
   SELECT * FROM ged2.geographic_region
--- WHERE id=111
--- WHERE tot_pop IS NULL
+-- WHERE id=244
+-- WHERE id IN (111,244)
+ --WHERE id=111
+ WHERE tot_pop IS NULL
 ),
 grid AS (
   SELECT gr.id AS gr_id, (ged2.get_region_grid(gr)).* FROM gr
@@ -13,7 +15,16 @@ grid AS (
 totals AS (
 SELECT gr_id,
   SUM(grid.pop_value) AS total_population, COUNT(*) AS total_grid_count ,
-  ST_SetSRID(ST_Extent(gp.the_geom),4326) AS bounding_box
+  CASE 
+  WHEN COUNT(*) = 1 THEN
+    -- Handle case where region is only a single cell: 
+    -- add a buffer with a half-cell radius and recalculate extent
+    ST_SetSRID(ST_Extent(ST_Buffer(gp.the_geom,0.0042)),4326) 
+  WHEN COUNT(*) = 0 THEN
+    NULL
+  ELSE
+    ST_SetSRID(ST_Extent(gp.the_geom),4326) 
+  END AS bounding_box
   FROM grid
   JOIN ged2.grid_point gp ON gp.id=grid.grid_point_id
   GROUP BY gr_id
@@ -23,12 +34,23 @@ SELECT gr_id,
   -- ST_XMax(bounding_box) AS xmax, ST_YMax(bounding_box) AS ymax 
 --  FROM totals;
 
+-- RAISE NOTICE 'gr_id=% geometrytype=%, text=%', gr_id, geometrytype(bounding_box), ST_AsText(bounding_box);
 --
 -- Update ged2.geographic_region to add values from totals
 --
+
 UPDATE ged2.geographic_region gr 
    SET tot_pop_source='GED/GRUMP', tot_pop_date=CURRENT_DATE, 
        tot_pop=totals.total_population,
        tot_grid_count=totals.total_grid_count,
        bounding_box=totals.bounding_box
-  FROM totals WHERE totals.gr_id=gr.id
+  FROM totals 
+ WHERE totals.gr_id=gr.id;
+
+-- SELECT 
+-- 	totals.gr_id AS gr_id,
+-- 	totals.total_population AS tot_pop, 
+-- 	totals.total_grid_count AS tot_grid_count,
+-- 	totals.bounding_box AS bounding_box
+--  INTO paul.tot_pop_grid_count
+--  FROM totals 
